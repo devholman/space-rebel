4fb832ae9ce8ce12b71bd6b34b7471b6719ae7c2
dev.holman <devyn.holman@yahoo.com>
2020-06-21 09:39:54 -0500

    fixed particles background

---
+++ public/component---src-pages-404-js-60b845208834ffdae906.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{w2l6:function(n,e,t){"use strict";t.r(e);var p=t("q1tI"),u=t.n(p);e.default=function(){return u.a.createElement("div",null,"Opps!")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-60b845208834ffdae906.js.map
